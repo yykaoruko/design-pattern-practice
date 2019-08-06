3d0b2cb003c7c0b300830f71547f8e977426e7c7
yamamoto kaoruko <kaoruko@diffeasy.com>
2019-08-06 23:17:36 +0900

    feat: iterator

---
+++ src/iterator/index.js
@@ -0,0 +1,82 @@
+;
+;
+var Book = /** @class */ (function () {
+    function Book(name) {
+        if (name === void 0) { name = undefined; }
+        this._name = name;
+    }
+    ;
+    Book.prototype.getName = function () {
+        return this._name;
+    };
+    ;
+    return Book;
+}());
+;
+var BookShelf = /** @class */ (function () {
+    function BookShelf(maxsize) {
+        this._books = [];
+        this._last = 0;
+        for (var i = 0; i < maxsize; i++) {
+            this._books.push(new Book());
+        }
+        console.log(this._books);
+    }
+    ;
+    BookShelf.prototype.getBookAt = function (index) {
+        return this._books[index];
+    };
+    ;
+    BookShelf.prototype.appendBook = function (book) {
+        this._books[this._last] = book;
+        this._last++;
+    };
+    ;
+    BookShelf.prototype.getLength = function () {
+        return this._last;
+    };
+    ;
+    BookShelf.prototype.iterator = function () {
+        return new BookShelfIterator(this);
+    };
+    ;
+    return BookShelf;
+}());
+;
+var BookShelfIterator = /** @class */ (function () {
+    function BookShelfIterator(bookShelf) {
+        this._bookShelf = bookShelf;
+        this._index = 0;
+    }
+    ;
+    BookShelfIterator.prototype.hasNext = function () {
+        if (this._index < this._bookShelf.getLength()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+        ;
+    };
+    ;
+    BookShelfIterator.prototype.next = function () {
+        var book = this._bookShelf.getBookAt(this._index);
+        this._index++;
+        return book;
+    };
+    ;
+    return BookShelfIterator;
+}());
+;
+var bookShelf = new BookShelf(4);
+bookShelf.appendBook(new Book('a'));
+bookShelf.appendBook(new Book('b'));
+bookShelf.appendBook(new Book('c'));
+bookShelf.appendBook(new Book('d'));
+bookShelf.appendBook(new Book('e'));
+var it = bookShelf.iterator();
+while (it.hasNext()) {
+    var book = it.next();
+    // console.log(book.getName());
+}
+;
---
+++ src/iterator/index.ts
@@ -0,0 +1,77 @@
+interface MyIterator {
+  hasNext(): boolean;
+  next(): unknown;
+}
+
+interface Aggregate {
+  iterator(): MyIterator;
+}
+
+class Book {
+  private _name: string;
+  constructor(name = undefined) {
+    this._name = name;
+  }
+  getName() {
+    return this._name;
+  }
+}
+
+class BookShelf implements Aggregate {
+  private _books: Book[] = [];
+  private _last: number = 0;
+  constructor(maxsize: number) {
+    for (let i = 0; i < maxsize; i++) {
+      this._books.push(new Book());
+    }
+    console.log(this._books);
+  }
+  getBookAt(index: number): Book {
+    return this._books[index];
+  }
+  appendBook(book: Book): void {
+    this._books[this._last] = book;
+    this._last++;
+  }
+  getLength(): number {
+    return this._last;
+  }
+  iterator(): BookShelfIterator {
+    return new BookShelfIterator(this);
+  }
+}
+
+class BookShelfIterator implements MyIterator {
+  private _bookShelf: BookShelf;
+  private _index: number;
+  constructor(bookShelf: BookShelf) {
+    this._bookShelf = bookShelf;
+    this._index = 0;
+  }
+  hasNext(): boolean {
+    if (this._index < this._bookShelf.getLength()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  next(): Book {
+    const book: Book = this._bookShelf.getBookAt(this._index);
+    this._index++;
+    return book;
+  }
+}
+
+const bookShelf = new BookShelf(4);
+
+bookShelf.appendBook(new Book('a'));
+bookShelf.appendBook(new Book('b'));
+bookShelf.appendBook(new Book('c'));
+bookShelf.appendBook(new Book('d'));
+bookShelf.appendBook(new Book('e'));
+
+const it = bookShelf.iterator();
+while (it.hasNext()) {
+  const book = it.next();
+  console.log(book.getName());
+}
