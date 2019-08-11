753e16602f63e6185a836822af5983e8c77c1767
yamamoto kaoruko <kaoruko@diffeasy.com>
2019-08-12 02:41:11 +0900

    chore: ESLintの設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    es6: true
   },
   extends: ["eslint:recommended", "plugin:prettier/recommended"],
   plugins: ["@typescript-eslint", "prettier"],
